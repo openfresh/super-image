d6a786db68a474063e9ac1f8fefd3f6688ed45a7
1000ch <shogo.sensui@gmail.com>
2017-08-09 17:46:12 +0900

    Add tests

--- test/index.js
+++ test/index.js
@@ -5,19 +5,20 @@ import * as React from 'react';
 import * as TestUtils from 'react-dom/test-utils';
 import SuperImage from '../src';
 
-describe('SuperImage', () => {
+describe('SuperImage without fallback', () => {
   it('should return `<img>` element', () => {
     const superImage = TestUtils.renderIntoDocument(<SuperImage src="" />);
     const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'img');
 
-    assert(node.tagName === 'IMG');
+    assert.equal(node.tagName, 'IMG');
+    assert.equal(node.style.objectFit, '');
   });
 
-  it('should return `<div>` element when `SuperImage` has `fitFallback` property', () => {
-    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback src="" fit="cover" />);
-    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+  it('should have expected `alt`', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage alt="foo" src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'img');
 
-    assert(node.tagName === 'DIV');
+    assert.equal(node.alt, 'foo');
   });
 
   it('should have an empty `alt` when the `alt` attribute does not exist', () => {
@@ -28,9 +29,73 @@ describe('SuperImage', () => {
   });
 
   it('should have expected `className`', () => {
-    const superImage = TestUtils.renderIntoDocument(<SuperImage src="" className="a" />);
+    const superImage = TestUtils.renderIntoDocument(<SuperImage src="" className="foo" />);
     const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'img');
 
-    assert(node.className === 'a');
+    assert.equal(node.className, 'foo');
+  });
+
+  it('should have `width` and `height` with 100% when `flexible` attribute exists', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage flexible src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'img');
+
+    assert.equal(node.style.width, '100%');
+    assert.equal(node.style.height, '100%');
+  });
+
+  it('should have `objectFit` when `fit` attribute exists', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fit="contain" src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'img');
+
+    assert.equal(node.style.objectFit, 'contain');
+  });
+});
+
+describe('SuperImage with fallback', () => {
+  it('should return `<div>` element when `SuperImage` has `fitFallback` property', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+
+    assert.equal(node.tagName, 'DIV');
+    assert.equal(node.style.backgroundSize, '');
+    assert.equal(node.style.display, 'inline-block');
+    assert.equal(node.style.backgroundRepeat, 'no-repeat');
+    assert.equal(node.style.backgroundPosition, 'center center');
+  });
+
+  it('should have expected `aria-label`', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback alt="foo" src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+
+    assert.equal(node.getAttribute('aria-label'), 'foo');
+  });
+
+  it('should have an empty `aria-label` when the `alt` attribute does not exist', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+
+    assert(node.hasAttribute('aria-label'));
+  });
+
+  it('should have expected `className`', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback src="" className="foo" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+
+    assert.equal(node.className, 'foo');
+  });
+
+  it('should have `width` and `height` with 100% when `flexible` attribute exists', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback flexible width="10" height="10" src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+
+    assert.equal(node.style.width, '100%');
+    assert.equal(node.style.height, '100%');
+  });
+
+  it('should have `backgroundSize` when `fit` attribute exists', () => {
+    const superImage = TestUtils.renderIntoDocument(<SuperImage fitFallback fit="contain" src="" />);
+    const node = TestUtils.findRenderedDOMComponentWithTag(superImage, 'div');
+
+    assert.equal(node.style.backgroundSize, 'contain');
   });
 });
